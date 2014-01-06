373597da5d6f506cd639e52eba9aa1d5da6bdba4
bsabuncu <borasabuncu@gmail.com>
2014-01-06 16:42:26 -0500

    Like button is fixed, not possible to like an app twice.

--- public/javascripts/framescript.js
+++ public/javascripts/framescript.js
@@ -8,13 +8,27 @@ $('#rating').click(function(eventObject) {
 	eventObject.preventDefault();
 	var s = $("meta[name=locale]").attr("content");
     console.log(s);	
-	$.ajax({
+    var appName = $("#appName").attr("value");
+    var s = "";
+    var liked = new Array();
+    for (var i = 0; i < cookies.length; i++) {
+		var curCookie = cookies[i].trim();
+		liked.push(curCookie);
+    }
+    liked.push(appName);
+    var value = liked[0];
+    if(liked.length > 1)
+    	value = liked.join(',');
+    document.cookie = value;
+    $.ajax({
 		type: 'POST' ,// added,
 		url:"/updateRating" ,
 		dataType: 'json',
 		data: {objectData: s} ,
 		success: function (data) {
 			var ret = jQuery.parseJSON(data);
+			
+			
 		},
 		error: function (xhr, status, error) {
 			console.log('Error: ' + error.message);
--- routes/index.js
+++ routes/index.js
@@ -4,34 +4,36 @@ var db = require('../node_modules/Database_functions.js');
  */
 exports.updateRating = function(req,res){
 	var object = (req.body.objectData);
-	console.log(object);
+	console.log(object); 
 	db.updateRating(object,function(){
 		console.log("rating updated");
 	});
-}
+};
+
 exports.frame = function(req, res){
   console.log(req.query);
   var link = req.query.user.link;
   console.log(link);
   var object = -1;
-  db.getApp(link, object);
-  var value = -1;
-  if(object != -1 )
+  object = db.getApp(link);
+  var value = -1, name = "";
+  if(object != -1 ) {
 	  value = object.getRating();
-  res.render('frame',{frameSrc: link, rating: value});
-  
-}
-exports.home= function(req,res){
-		db.getAllApps(function(data){
-			data.sort(function(a,b){
-				if(a.rating == undefined)
-					a.rating = 0;
-				if(b.rating == undefined)
-					b.rating = 0;
-				return b.rating-a.rating;
-			});
-			res.render('index', {myObj: data} );
-		});
+	  name = object.getName();
+  }
+  res.render('frame',{frameSrc: link, rating: value, curApp:name}); 
+};
 
-}
+exports.home= function(req,res){
+	db.getAllApps(function(data){
+		data.sort(function(a,b){
+			if(a.rating == undefined)
+				a.rating = 0;
+			if(b.rating == undefined)
+				b.rating = 0;
+			return b.rating-a.rating;
+		});
+		res.render('index', {myObj: data} );
+	});
+};
 
